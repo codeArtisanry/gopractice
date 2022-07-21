6fcff595243c639b9aadf337fc3a15975d5060e5
Vatsal <chauhanvatsal55@gmail.com>
2022-07-21 12:34:47 +0530

    fix: to public package

--- gopractice.go
+++ gopractice.go
@@ -1,7 +1,7 @@
 package gopractice
 
 //create a func that returns hash from string
-func hash(str string) uint32 {
+func Hash(str string) uint32 {
 	var hash uint32
 	for _, c := range str {
 		hash = hash*31 + uint32(c)
