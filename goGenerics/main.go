d19ff7a62567fd0d9d765014ee95eaca55db820c
Vatsal <chauhanvatsal55@gmail.com>
2022-11-09 14:46:31 +0530

    add go generics

---
+++ goGenerics/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func newGenericFunc[age int64 | float64](myAge age) {
+	fmt.Println(myAge)
+}
+
+func main() {
+
+	var testAge int64 = 23
+	var testAge2 float64 = 24.55
+
+	newGenericFunc(testAge)
+	newGenericFunc(testAge2)
+
+}
