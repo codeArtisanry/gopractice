8e768d91b1f0965e5b0ad38f8b57b173beafd0fe
Vatsal <chauhanvatsal55@gmail.com>
2022-11-09 14:47:02 +0530

    reset main.go file

--- app.go
+++ app.go
@@ -2,15 +2,6 @@ package main
 
 import "fmt"
 
-func newGenericFunc[age int64 | float64](myAge age) {
-	fmt.Println(myAge)
-}
-
 func main() {
-	fmt.Println("Inside Main")
-	var testAge int64 = 23
-	var testAge2 float64 = 24.55
-
-	newGenericFunc(testAge)
-	newGenericFunc(testAge2)
+	fmt.Println("Hello, go playground")
 }
