6cef3d14e460391666b26df070a7702f91882101
Vatsal Chauhan <87218847+codeArtisanry@users.noreply.github.com>
2024-06-02 09:51:45 +0530

    Day-!

---
+++ examples/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var choice int
+	fmt.Println("Enter 1 to calculate area and circumference of a circle")
+	fmt.Println("Enter 2 to convert celsius into fahrenheit")
+	fmt.Println("Enter 3 to use simple calculator")
+  fmt.Println("Enter 4 to swap two variables values")
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case 1:
+		calcAreaAndCircumference()
+	case 2:
+		convertCelsiusIntoFahrenheit()
+	case 3:
+		simpleCalculator()
+  case 4:
+    swapTwoVariablesValuesWithoutTemp()
+	default:
+		fmt.Println("Invalid choice")
+	}
+}
+
+// +++++++++++++++++++++++++++++++++ Day 1 +++++++++++++++++++++++++++++++++
+
+func calcAreaAndCircumference() {
+	var radius float64
+	var area, circumference float64
+
+	fmt.Printf("Enter the radius of the circle:")
+	fmt.Scanln(&radius)
+
+	area = math.Pi * radius * radius
+	circumference = 2 * math.Pi * radius
+
+	fmt.Printf("Aream of the circle: %.2f\n", area)
+	fmt.Printf("circumference of the circle: %.2f\n", circumference)
+}
+
+func convertCelsiusIntoFahrenheit() {
+	var celsius, fahrenheit float64
+
+	fmt.Println("Please enter the temperature in celsius: ")
+	fmt.Scanln(&celsius)
+
+	fahrenheit = (celsius * 9 / 5) + 32
+
+	fmt.Printf("%.2f celsius is equal to %.2f fahrenheit\n", celsius, fahrenheit)
+
+	fmt.Println("Enter the temperature in fahrenheit: ")
+	fmt.Scanln(&fahrenheit)
+
+	celsius = (fahrenheit - 32) * 5 / 9
+
+	fmt.Printf("%.2f fahrenheit = %.2f Celsius\n", fahrenheit, celsius)
+}
+
+func simpleCalculator() {
+	var num1, num2 float64
+	var operator string
+
+	fmt.Println("Enter the first number: ")
+	fmt.Scanln(&num1)
+
+	fmt.Println("Enter the second number: ")
+	fmt.Scanln(&num2)
+
+	fmt.Println("Enter the operator (+, -, *, /): ")
+	fmt.Scanln(&operator)
+
+	switch operator {
+	case "+":
+		fmt.Printf("%.2f + %.2f = %.2f\n", num1, num2, num1+num2)
+	case "-":
+		fmt.Printf("%.2f - %.2f = %.2f\n", num1, num2, num1-num2)
+	case "*":
+		fmt.Printf("%.2f * %.2f = %.2f\n", num1, num2, num1*num2)
+	case "/":
+		fmt.Printf("%.2f / %.2f = %.2f\n", num1, num2, num1/num2)
+	default:
+		fmt.Println("Invalid operator")
+	}
+}
+
+func swapTwoVariablesValuesWithoutTemp() {
+  var a, b float64
+
+  fmt.Println("Enter the value of a: ")
+  fmt.Scanln(&a)
+
+  fmt.Println("Enter the value of b: ")
+  fmt.Scanln(&b)
+
+  a = a + b
+  b = a - b
+  a = a - b
+
+  fmt.Printf("After swapping, a = %.2f and b = %.2f\n", a, b)
+
+}
